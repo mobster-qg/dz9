1b006ea0a4f5d87ade0d7a2e7eea666e733ce378
Achelov efim <mobster-qg@@mail.ru>
2022-12-23 14:30:19 +0700

    dz64

---
+++ dz64/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+// M = 5; N = 3 -> "5, 4, 3"
+// M = 8; N = 4. -> "8, 7, 6, 5, 4"
+using System;
+using static System.Console;
+
+
+Clear();
+Write("Введите M: ");
+int m=int.Parse(ReadLine());
+Write("Введите N: ");
+int n=int.Parse(ReadLine());
+
+WriteLine(PrintNumbers(m,n));
+
+string PrintNumbers(int start, int end)
+{
+    if (start == end) 
+        return start.ToString();
+    return (start + " " +PrintNumbers(start - 1, end) );
+    Write($"{start}");
+}
